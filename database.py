155aba62871a9c7f2b6b43b32ec838c87ae43150
shockpast <shockpast@gmail.com>
2023-03-07 20:55:01 +0300

    feat: database as seperate module

---
+++ database.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+sqlite = sqlite3.connect("server.db", check_same_thread=False)
+cursor = sqlite.cursor()
+
+def init():
+    cursor.execute("CREATE TABLE IF NOT EXISTS shortened_urls ('full' varchar, 'short' varchar, 'clicks' INTEGER)")
+
+def exec(sql: str, params: ...):
+	cursor.execute(sql, params)
+	sqlite.commit()
+
+	return cursor
